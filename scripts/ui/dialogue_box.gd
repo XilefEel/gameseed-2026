976a8c2e833378e2d80de054b4dcdc4aa24c196a
Felix Lee <felixlee175@gmail.com>
2026-06-26 21:53:14 +0700

    added talk sfx with randomized pitch for dialogues

--- scripts/ui/dialogue_box.gd
+++ scripts/ui/dialogue_box.gd
@@ -6,6 +6,7 @@ signal finished
 @onready var dialogue_label: Label = %DialogueText
 @onready var character_sprite: TextureRect = %CharacterSprite
 @onready var sprite_container: HBoxContainer = %SpriteContainer
+@onready var sfx_talk: AudioStreamPlayer2D = %SFX_Talk
 
 var lines: Array = []
 var current_line := 0
@@ -17,6 +18,8 @@ const CHARACTER_SPRITES = {
 	"Cahyo": preload("res://assets/sprites/player/cahyo.png"),
 	"Boss": preload("res://assets/sprites/player/boss.png")
 }
+
+const SILENT_CHARS = [".", ",", "!", "?", "\n"]
 
 func play(dialogue: Array) -> void:
 	lines = dialogue
@@ -45,6 +48,12 @@ func show_line() -> void:
 
 	while dialogue_label.visible_characters < dialogue_label.text.length():
 		dialogue_label.visible_characters += 1
+		
+		var c = dialogue_label.text[dialogue_label.visible_characters - 1]
+		if c not in SILENT_CHARS:
+			sfx_talk.pitch_scale = randf_range(0.9, 1.1)
+			sfx_talk.play()
+
 		await get_tree().create_timer(CHAR_DELAY).timeout
 
 	is_typing = false
